41a25bda70809bf23adec35b10ff24d3497eaad2
mkustigian23 <mkustigian23@gmail.com>
2026-02-02 20:48:09 -0500

    Sprint 1 error fix

--- testSpeech.py
+++ testSpeech.py
@@ -4,8 +4,8 @@ import json
 from vosk import Model, KaldiRecognizer
 import wave
 
-
-from speechToText import listen_and_transcribe  # Fixed now should pass all tests
+from speechToText import listen_and_transcribe
+#Fixed now should pass all tests
 
 
 class TestVoskTranscription(unittest.TestCase):
@@ -22,7 +22,7 @@ class TestVoskTranscription(unittest.TestCase):
 
         self.assertTrue(os.path.exists(audio_file), "Test audio file missing in CI")
 
-        text = transcribe_file(audio_file, self.model_path)
+        text = listen_and_transcribe(audio_file, self.model_path)
 
         text = text.lower().strip()
         print("Recognized:", text)
